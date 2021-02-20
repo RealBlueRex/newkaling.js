e2ad4853b1c90bd556e8b05e11f5dc5cd537d444
app6460 <sk62926670@gmail.com>
2021-02-20 17:11:15 +0900

    파일에 있는 사진파일전송추가 (테스트안해봄)

--- renewkaling.js
+++ renewkaling.js
@@ -227,6 +227,22 @@ module.exports = (function () {
             }
         });
     }
+    Kakao.prototype.sendfileImage = function(room, path, Text, dec){
+        var url='http://dn-m.talk.kakao.com/'+(function upload(type, path) {
+       	 try{
+                var file = new java.io.File(path);
+                var fileInputStream = new java.io.FileInputStream(file);
+            }catch(e)return null;
+        var res = org.jsoup.Jsoup.connect("https://up-m.talk.kakao.com/upload")
+        .header("A", "An/9.0.0/ko")
+        .data("attachment_type", type)
+        .data("user_id", "-1")
+        .data("file", file.getName(), fileInputStream)
+        .ignoreHttpErrors(true).post().text();
+        return res[0]=='5'?null:res
+        })("image/jpeg", path);
+        this.sendImage(room, url, Text, dec);
+    }
 
     return Kakao
 })();
